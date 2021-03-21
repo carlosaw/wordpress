ad05813d7f67ee0a3a1f530977fcb8e956fc07f7
unknown <carlosfreevg@gmail.com>
2021-03-21 08:26:47 -0400

    Exemplo-OK

--- wp-content/themes/primeirotema/include/setup.php
+++ wp-content/themes/primeirotema/include/setup.php
@@ -9,6 +9,10 @@ function ap_after_setup() {
 
   add_theme_support('post-thumbnails');
 
+  add_theme_support('title-tag');
+
+  add_theme_support('custom-logo');
+
   register_nav_menu('primary', __('Primary Menu', 'primeirotema'));
   //register_nav_menu('footer', 'Menu Rodapé');
 }
